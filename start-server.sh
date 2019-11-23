0590e52d4085fec2030c3d242290e5c935520a54
rbrecheisen <r.brecheisen@maastrichtuniversity.nl>
2019-11-23 10:53:23 +0100

    Removed requirements.txt and put pip installs in startup script

--- start-server.sh
+++ start-server.sh
@@ -9,17 +9,21 @@ docker run -d --name jupyterlab \
 	-v $(pwd)/../work:/home/jovyan/work \
 	-v $(pwd)/../ssl/espresso.maastrichtuniversity.nl.key:/etc/ssl/jupyter/espresso.maastrichtuniversity.nl.key \
 	-v $(pwd)/../ssl/espresso.maastrichtuniversity.nl.crt:/etc/ssl/jupyter/espresso.maastrichtuniversity.nl.crt \
-	-v $(pwd)/requirements.txt:/home/jovyan/requirements.txt \
 	jupyter/datascience-notebook start-notebook.sh \
 		--NotebookApp.keyfile=/etc/ssl/jupyter/espresso.maastrichtuniversity.nl.key \
 		--NotebookApp.certfile=/etc/ssl/jupyter/espresso.maastrichtuniversity.nl.crt
 
-docker exec -it jupyterlab bash -c "pip install -r requirements.txt"
-
+docker exec -it jupyterlab bash -c "pip install xnat"
+docker exec -it jupyterlab bash -c "pip install pymongo"
+docker exec -it jupyterlab bash -c "pip install ipywidgets"
+docker exec -it jupyterlab bash -c "pip install RISE"
 docker exec -it jupyterlab bash -c "pip install https://github.com/ipython-contrib/jupyter_contrib_nbextensions/tarball/master"
 docker exec -it jupyterlab bash -c "pip install jupyter_nbextensions_configurator"
+
 docker exec -it jupyterlab bash -c "jupyter contrib nbextension install --user"
 docker exec -it jupyterlab bash -c "jupyter nbextensions_configurator enable --user"
+docker exec -it jupyterlab bash -c "jupyter-nbextension install rise --py --sys-prefix"
+docker exec -it jupyterlab bash -c "jupyter-nbextension enable rise --py --sys-prefix"
 
 cd restheart
 
