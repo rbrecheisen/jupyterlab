1f16863c2ba651fcd5c883988853747b7c4bdc40
rbrecheisen <r.brecheisen@maastrichtuniversity.nl>
2019-12-19 12:54:06 +0100

    Added Keras and TensorFlow to requirements

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,7 @@
 FROM jupyter/datascience-notebook
 
-ADD requirements.txt /requirements.txt
-
 USER root
 
 RUN apt update && \
     apt install -y vim && \
-    apt install -y pkg-config && \
-    pip install -r /requirements.txt
-
+    apt install -y pkg-config
--- notebook-start.sh
+++ notebook-start.sh
@@ -3,4 +3,6 @@
 # WARNING: Do not edit this script. It only runs inside the Docker container and serves
 # to install additional Python packages and startup JupyterLab
 
+pip install -r /home/jovyan/requirements.txt
+
 /usr/local/bin/start-notebook.sh --NotebookApp.keyfile=/etc/ssl/jupyter/espresso.key --NotebookApp.certfile=/etc/ssl/jupyter/espresso.crt
